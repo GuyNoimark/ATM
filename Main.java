3e7602a789fc86f1a7a0822970760d158eff2de2
Guy Noimark <guy.noimark27@gmai.com>
2021-01-30 16:47:51 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main extends User {
+    public static void main(String[] args) {
+
+        User me = new User("Tom", "tom@gmail.com", "1234");
+        System.out.println(me.userName);
+    }
+
+}
---
+++ User.java
@@ -0,0 +1,16 @@
+public class User {
+
+    String userName;
+    String userEmail;
+    String userPassword;
+
+    public User(){};
+    public User(String name, String email, String password) {
+        this.userName = name;
+        this.userEmail = email;
+        this.userPassword = password;
+
+
+    }
+
+}
